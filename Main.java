b67b891cb3964a72e4cbbe3a5614dd2e62b8b628
Exwithe <52176228+Exwithe@users.noreply.github.com>
2019-10-06 20:13:28 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Number!");
+        int number = 0;
+        boolean hasNextInt = scanner.hasNextInt();
+        if(hasNextInt) {
+            number = scanner.nextInt();
+            scanner.nextLine();
+        } else {
+            System.out.println("Diagnose Hurensohn");
+        }
+        int [] oneToHundred = new int[number];
+        for(int i = 0; i < number; i++) {
+            oneToHundred[i] = i;
+        }
+        for(int i = 0; i <= number; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
